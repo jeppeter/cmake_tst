b582dfcfcde13493a8a16bcdad4a1b957be387f2
jeppeter <jeppeter@gmail.com>
2015-11-06 20:25:50 +0800

    to try run cmake

---
+++ runcmake.bat
@@ -0,0 +1,3 @@
+cmake.exe -C ..\cmake_tst -G "Visual Studio 14 2015 Win64" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/cl.exe"
+
+REM cmake.exe -C ..\cmake_tst -G "Visual Studio 14 2015 Win64"
