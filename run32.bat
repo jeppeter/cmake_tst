d237448ac1b9f1c626211cc2addf778d5827e9a9
jeppeter <jeppeter@gmail.com>
2015-11-07 13:53:31 +0800

    to make the run32 and run64 ok

---
+++ run32.bat
@@ -0,0 +1,1 @@
+cmake.exe -G "Visual Studio 14 2015" -DCMAKE_CONFIGURATION_TYPES="Release;Debug"  ..
--- runcmake.bat
+++
@@ -1,1 +0,0 @@
-cmake.exe -G "Visual Studio 14 2015 Win64" -DCMAKE_CONFIGURATION_TYPES="Release;Debug" ..
