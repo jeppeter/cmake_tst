7ed23d03c015aad06507c871d1df4126846e186e
jeppeter <jeppeter@gmail.com>
2015-11-07 13:05:11 +0800

    to make the build ok

--- runcmake.bat
+++ runcmake.bat
@@ -1,3 +1,1 @@
-cmake.exe -C ..\ -G "Visual Studio 14 2015 Win64" -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/amd64/cl.exe"
-
-REM cmake.exe -C ..\cmake_tst -G "Visual Studio 14 2015 Win64"
+cmake.exe -G "Visual Studio 14 2015 Win64" -DCMAKE_CONFIGURATION_TYPES="Release;Debug" ..
